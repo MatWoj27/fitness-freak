49cbabe3949f96b419f98d20cf15c8962f4ec91c
Mateusz <226069@student.pwr.edu.pl>
2020-12-10 23:56:45 +0100

    Simple navigation to weight activity added

--- app/src/main/java/com/example/fitnessfreak/MainActivity.kt
+++ app/src/main/java/com/example/fitnessfreak/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.fitnessfreak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fitnessfreak.weightworkout.WeightWorkoutActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        weight_workouts_btn.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    WeightWorkoutActivity::class.java
+                )
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/WeightWorkoutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fitnessfreak.weightworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnessfreak.R
+
+class WeightWorkoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weight_workout)
+    }
+}
