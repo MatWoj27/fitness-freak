27c56c15542c6349397296e26ab2fa8fedfb04b1
Mateusz <226069@student.pwr.edu.pl>
2020-12-11 23:57:13 +0100

    Toolbar added to weight workout activity

--- app/src/main/java/com/example/fitnessfreak/weightworkout/WeightWorkoutActivity.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/WeightWorkoutActivity.kt
@@ -3,11 +3,21 @@ package com.example.fitnessfreak.weightworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitnessfreak.R
+import kotlinx.android.synthetic.main.activity_weight_workout.*
 
 class WeightWorkoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weight_workout)
+        presetToolbar()
+    }
+
+    private fun presetToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
     }
 }
