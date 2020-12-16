723b01d084d56716f90ae7eb99e714e289403dd4
Mateusz <226069@student.pwr.edu.pl>
2020-12-16 23:58:02 +0100

    Dividers added to the list of the weight exercises

---
+++ app/src/main/java/com/example/fitnessfreak/RecyclerViewExtension.kt
@@ -0,0 +1,17 @@
+package com.example.fitnessfreak
+
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+
+fun RecyclerView.setDivider(@DrawableRes drawableRes: Int) {
+    ContextCompat.getDrawable(this.context, drawableRes)?.let {
+        val divider = DividerItemDecoration(
+            this.context,
+            DividerItemDecoration.VERTICAL
+        )
+        divider.setDrawable(it)
+        addItemDecoration(divider)
+    }
+}
--- app/src/main/java/com/example/fitnessfreak/weightworkout/WeightWorkoutActivity.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/WeightWorkoutActivity.kt
@@ -3,7 +3,9 @@ package com.example.fitnessfreak.weightworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitnessfreak.R
+import com.example.fitnessfreak.setDivider
 import kotlinx.android.synthetic.main.activity_weight_workout.*
 
 class WeightWorkoutActivity : AppCompatActivity() {
@@ -15,6 +17,7 @@ class WeightWorkoutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_weight_workout)
         presetToolbar()
         viewModel = ViewModelProvider(this).get(WeightWorkoutViewModel::class.java)
+        presetExercisesList()
     }
 
     private fun presetToolbar() {
@@ -23,5 +26,14 @@ class WeightWorkoutActivity : AppCompatActivity() {
             setDisplayShowTitleEnabled(false)
         }
         up_navigation.setOnClickListener { onNavigateUp() }
+    }
+
+    private fun presetExercisesList() {
+        exerciseAdapter = ExerciseAdapter(mutableListOf())
+        exercise_list.apply {
+            layoutManager = LinearLayoutManager(this@WeightWorkoutActivity)
+            adapter = exerciseAdapter
+            setDivider(R.drawable.horizontal_divider)
+        }
     }
 }
