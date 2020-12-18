6965bbc9c5d0c4013afa0a896a55085617a7c6aa
Mateusz <226069@student.pwr.edu.pl>
2020-12-18 23:45:02 +0100

    Method providing exercise volume implemented

--- app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightExercise.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightExercise.kt
@@ -3,7 +3,11 @@ package com.example.fitnessfreak.weightworkout.models
 data class WeightExercise(
     val name: String,
     val sets: List<WeightSet>
-)
+) {
+    fun getExerciseVolume() = sets.sumBy { weightSet ->
+        weightSet.reps * weightSet.weight
+    }
+}
 
 data class WeightSet(
     val weight: Int,
