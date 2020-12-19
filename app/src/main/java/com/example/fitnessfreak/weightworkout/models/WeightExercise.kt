ba04b795d1852eabd93424df29f736099a655567
Mateusz <226069@student.pwr.edu.pl>
2020-12-19 22:37:38 +0100

    Method providing sets counter text implemented

--- app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightExercise.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightExercise.kt
@@ -1,11 +1,23 @@
 package com.example.fitnessfreak.weightworkout.models
 
+import android.content.Context
+import com.example.fitnessfreak.R
+
 data class WeightExercise(
     val name: String,
     val sets: List<WeightSet>
 ) {
     fun getExerciseVolume() = sets.sumBy { weightSet ->
         weightSet.reps * weightSet.weight
+    }
+
+    fun getSetsColumnText(context: Context): String {
+        val setsString = context.getString(R.string.sets)
+        return if (sets.isNotEmpty()) {
+            "${sets.size} ${setsString.toLowerCase()}"
+        } else {
+            setsString
+        }
     }
 }
 
