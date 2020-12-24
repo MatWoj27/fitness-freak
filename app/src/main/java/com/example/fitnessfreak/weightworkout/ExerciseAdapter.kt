7da37bb26a63b20355c8054d8e38e7b7de3093db
Mateusz <226069@student.pwr.edu.pl>
2020-12-24 23:44:21 +0100

    Oval background for exercise volume view

--- app/src/main/java/com/example/fitnessfreak/weightworkout/ExerciseAdapter.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/ExerciseAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.fitnessfreak.weightworkout
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessfreak.R
 import com.example.fitnessfreak.databinding.ExerciseItemBinding
 import com.example.fitnessfreak.weightworkout.models.WeightExercise
 
@@ -13,11 +16,38 @@ class ExerciseAdapter(private val exercises: MutableList<WeightExercise>) :
     class ExerciseViewHolder(private val binding: ExerciseItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            binding.exerciseVolume.addTextChangedListener(
+                object : TextWatcher {
+                    override fun afterTextChanged(p0: Editable?) {}
+
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) =
+                        resizeVolumeView()
+                }
+            )
+        }
+
         fun bind(exercise: WeightExercise) {
             binding.exercise = exercise
             binding.setsList.apply {
                 layoutManager = LinearLayoutManager(binding.root.context)
                 adapter = SetAdapter(exercise.sets)
+            }
+        }
+
+        private fun resizeVolumeView() {
+            binding.exerciseVolume.let {
+                it.measure(0, 0)
+                val width = it.measuredWidth
+                val height = it.measuredHeight
+                if (width > height) {
+                    it.height = width
+                } else {
+                    it.width = height
+                }
+                binding.exerciseVolume.setBackgroundResource(R.drawable.oval_background)
             }
         }
     }
