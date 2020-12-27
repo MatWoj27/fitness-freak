483d73a082cdd402c963745042679710ac3905e6
Mateusz <226069@student.pwr.edu.pl>
2020-12-27 15:16:19 +0100

    For some reason the type converter is not detected if annotation is applied to entity field

--- app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightWorkout.kt
+++ app/src/main/java/com/example/fitnessfreak/weightworkout/models/WeightWorkout.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.example.fitnessfreak.weightworkout.WeightWorkoutTypeConverters
 
+@TypeConverters(WeightWorkoutTypeConverters::class)
 @Entity(tableName = WeightWorkout.TABLE_NAME)
 data class WeightWorkout(
     @PrimaryKey(autoGenerate = true) val id: Long,
     val name: String,
     @ColumnInfo(name = DATE_COLUMN)
     val date: String,
-    @TypeConverters(WeightWorkoutTypeConverters::class)
     val exercises: List<WeightExercise>
 ) {
     companion object {
